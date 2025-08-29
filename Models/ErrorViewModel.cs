f547c230850b96527e867b3dd449480eb2b0862c
Jonathan L. <70529549+Hairo03@users.noreply.github.com>
2025-08-29 10:06:23 +0200

    Initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace dive_deep.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
