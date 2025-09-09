14821cdbdce6665ec586253c454f0e52227ac17f
Jonathan L. <70529549+Hairo03@users.noreply.github.com>
2025-09-09 12:15:30 +0200

    Tilføjede booking

---
+++ Controllers/BookingController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace dive_deep.Controllers
+{
+    public class BookingController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
