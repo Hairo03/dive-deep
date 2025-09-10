fcc7b31cfb36dde6800d7b71f96592af40029811
Marcus <marcuskristensen10@hotmail.com>
2025-09-10 11:25:45 +0200

    StartDate and EndDate works correctly

--- Controllers/BookingController.cs
+++ Controllers/BookingController.cs
@@ -1,12 +1,15 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using dive_deep.Models;
+using dive_deep.Persistence;
+using Microsoft.AspNetCore.Mvc;
 
 namespace dive_deep.Controllers
 {
     public class BookingController : Controller
     {
-        public IActionResult Index()
+        public IActionResult Index(int? id)
         {
-            return View();
+            Booking booking = new Booking() { Product = ProductRepo.GetProductById(id.HasValue ? id.Value : 0) };
+            return View(booking);
         }
     }
 }
--- Models/Booking.cs
+++ Models/Booking.cs
@@ -1,12 +1,23 @@
-﻿namespace dive_deep.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace dive_deep.Models
 {
     public class Booking
     {
+        
         public int Id { get; set; }
+
+        [Required, ]
         public DateTime StartDate { get; set; }
         public DateTime EndDate { get; set; }
         public double PricePerDay { get; set; }
         public Package? Package { get; set; }
         public Product? Product { get; set; }
+
+        public Booking()
+        {
+            StartDate = DateTime.Now;
+        }
+
     }
 }
