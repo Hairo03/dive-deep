fbb1fd48461748c5a4ade7269623f05f3e2dd846
Marcus <marcuskristensen10@hotmail.com>
2025-09-10 17:17:21 +0200

    fjernet punktum fra required i modellen.

--- Models/Booking.cs
+++ Models/Booking.cs
@@ -7,7 +7,6 @@ namespace dive_deep.Models
         
         public int Id { get; set; }
 
-        [Required, ]
         public DateTime StartDate { get; set; }
         public DateTime EndDate { get; set; }
         public double PricePerDay { get; set; }
