4992e246f9c4f32ad2cccaeac7e8133525a108fd
Marcus <marcuskristensen10@hotmail.com>
2025-09-12 17:20:22 +0200

    small changes.

---
+++ Views/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace dive_deep.Views
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
